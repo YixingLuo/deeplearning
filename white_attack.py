aa9442161b3dd33aab5934691b104d74b68b5f29
luoyixing <luoyi_xing@126.com>
2019-03-21 23:07:28 +0800

    new

--- white_attack.py
+++ white_attack.py
@@ -1,11 +1,18 @@
 import foolbox
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
 
 keras.backend.set_learning_phase(0)
-kmodel = keras.models.load_model('model-keras.h5')
-fmodel = foolbox.models.KerasModel(kmodel, bounds=(0, 255),preprocess_fn=preprocess_input)
+#kmodel = keras.models.load_model('model-keras.h5')
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+kmodel = keras.models.model_from_json(loaded_model_json)
+preprocessing = (np.array([28,28,1]),1)
+
+fmodel = foolbox.models.KerasModel(kmodel, bounds=(0, 255),preprocessing=preprocessing)
 
 
 fashion_mnist = keras.datasets.fashion_mnist
